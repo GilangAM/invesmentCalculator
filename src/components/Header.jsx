b17a53c2465da35b5dd4ee59558ace907139d2bd
Gilang <gilangardhi87@gmail.com>
2025-03-26 09:12:12 +0700

    update all

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logo from '../assets/investment-calculator-logo.png'
+
+export default function(){
+    return <header id='header'>
+        <img src={logo} alt="logo showing money bag" />
+        <h1>Investment Calculator</h1>
+    </header>
+}
